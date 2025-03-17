69729949421a1ebfad773462d25f366dc71bef14
この中二病に爆焔を！ <xie.guigang@gcmodeller.org>
2025-03-17 10:23:29 +0800

    test success

--- src/test/Form1.vb
+++ src/test/Form1.vb
@@ -63,7 +63,7 @@ Public Class Form1
             colorSet:="jet",
             padding:="padding: 5% 10% 10% 10%;")
 
-        plot += geom_point(size:=12)
+        plot += geom_point(size:=5)
 
         view.ScaleFactor = 1.25
         view.PlotPadding = plot.ggplotTheme.padding
