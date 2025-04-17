2f8285da8d3a40825489d551a2e9c14978f15f4d
この中二病に爆焔を！ <xie.guigang@gcmodeller.org>
2025-04-18 01:29:35 +0800

    set axis range for character mapping

--- src/PlotView.vb
+++ src/PlotView.vb
@@ -76,12 +76,12 @@ Public Class PlotView
         If x.mapper = d3js.scale.MapperTypes.Continuous Then
             Me.x = x.ToNumeric
         Else
-            Throw New NotImplementedException()
+            Me.x = x.range
         End If
         If y.mapper = d3js.scale.MapperTypes.Continuous Then
             Me.y = y.ToNumeric
         Else
-            Throw New NotImplementedException()
+            Me.y = y.range
         End If
 
         m_ps = g.GetContextInfo
